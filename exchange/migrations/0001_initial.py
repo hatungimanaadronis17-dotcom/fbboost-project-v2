7ac0cf3d823e639c68efc429bf9201fa923986f0
hatungimanaadronis17-dotcom <hatungimanaadronis17@gmail.com>
2025-11-27 21:33:20 -0500

    Add exchange initial migration (fix import Balance)

--- exchange/migrations/0001_initial.py
+++ exchange/migrations/0001_initial.py
@@ -3,9 +3,11 @@ import django.db.models.deletion
 
 class Migration(migrations.Migration):
     initial = True
+
     dependencies = [
         ('auth', '0012_alter_user_first_name_max_length'),
     ]
+
     operations = [
         migrations.CreateModel(
             name='Balance',
@@ -20,12 +22,12 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('platform', models.CharField(choices=[('facebook', 'Facebook'), ('instagram', 'Instagram'), ('tiktok', 'TikTok'), ('youtube', 'YouTube')], max_length=20)),
-                ('task_url', models.URLField()),
-                ('coins_reward', models.IntegerField(default=10)),
+                ('task_url', models.URLField(max_length=500)),
+                ('coins_reward', models.IntegerField(default=0)),
                 ('completed', models.BooleanField(default=False)),
                 ('validated', models.BooleanField(default=False)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='auth.user')),
+                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='tasks_done', to='auth.user')),
             ],
         ),
         migrations.CreateModel(
