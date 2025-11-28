31dcdd0cd138ab64c1d9a925bafeed71af2635b0
hatungimanaadronis17-dotcom <hatungimanaadronis17@gmail.com>
2025-11-28 10:25:19 -0500

    Delete posts directory

--- posts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'posts'
-
-urlpatterns = [
-    path('', views.feed, name='feed'),
-    path('like/<int:pk>/', views.like_post, name='like'),
-]
