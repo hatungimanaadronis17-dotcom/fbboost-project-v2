b639479358b6d2c45ec75520c5787cc333fb7016
hatungimanaadronis17-dotcom <hatungimanaadronis17@gmail.com>
2025-11-27 22:01:14 -0500

    Update urls.py

--- fbboost/urls.py
+++ fbboost/urls.py
@@ -5,9 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('posts.urls')),
-    path('', include('users.urls')),
+    path('exchange/', include('exchange.urls')),
+    path('', include('exchange.urls')),  # page d’accueil = échange
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
