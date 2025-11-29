f80db08acb0f2d78fd5d6f622784121d3e1af888
hatungimanaadronis17-dotcom <hatungimanaadronis17@gmail.com>
2025-11-29 16:39:55 -0500

    Update urls.py

--- fbboost/urls.py
+++ fbboost/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import views
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -6,15 +5,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # 1. Auth Django → /login/ et /logout/ avec ton template
     path('', include('django.contrib.auth.urls')),
-
-    # 2. Tes apps (pas de namespace ici, on les met dans chaque app)
     path('', include('users.urls')),
     path('', include('exchange.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
