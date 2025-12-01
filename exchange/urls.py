572e962b971417e864149a2d8f9a7d07eacc3126
hatungimanaadronis17-dotcom <hatungimanaadronis17@gmail.com>
2025-11-30 20:37:27 -0500

    Update urls.py

--- exchange/urls.py
+++ exchange/urls.py
@@ -1,10 +1,8 @@
 app_name = 'exchange'
-from django.urls import path,include
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', include('exchange.views.home'), name='home'),                    # page d'accueil (échange)
+    path('', views.exchange_home, name='home'),          # ← CORRIGÉ
     path('submit/', views.submit_task, name='submit_task'),
-    # Toutes tes autres URLs de l'espace échange ici
-    # ex: path('task/<int:pk>/', views.task_detail, name='task_detail'),
 ]
