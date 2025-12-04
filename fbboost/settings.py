4ab8e4a3fbef3f3906602f1e8135df86c98bbd8a
hatungimanaadronis17-dotcom <hatungimanaadronis17@gmail.com>
2025-12-03 20:49:44 -0500

    Update settings.py

--- fbboost/settings.py
+++ fbboost/settings.py
@@ -45,7 +45,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context.processor.debug',
+                'django.template.context.processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
