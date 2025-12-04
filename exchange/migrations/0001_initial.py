2f9b85f649428f352daf99cb64b27bab2bd75f26
hatungimanaadronis17-dotcom <hatungimanaadronis17@gmail.com>
2025-12-03 22:19:09 -0500

    Update 0001_initial.py

--- exchange/migrations/0001_initial.py
+++ exchange/migrations/0001_initial.py
@@ -1,29 +1,22 @@
 from django.db import migrations, models
 import django.db.models.deletion
+from django.conf import settings
 
 class Migration(migrations.Migration):
 
     initial = True
 
     dependencies = [
-        # Si tu dépends de l'user auth
-        ('auth', '0012_alter_user_first_name_max_length'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Exchange',
+            name='Balance',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=100)),
-                ('url', models.URLField(max_length=500)),
-                ('api_key', models.CharField(max_length=200)),
-                ('api_secret', models.CharField(max_length=200)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('user', models.ForeignKey(
-                    on_delete=django.db.models.deletion.CASCADE,
-                    to='auth.user'
-                )),
+                ('coins', models.IntegerField(default=0)),
+                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
     ]
