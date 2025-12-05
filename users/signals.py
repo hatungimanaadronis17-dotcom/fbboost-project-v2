c0de027ce434b9e1d239a6b77e1e01cf94c3889b
hatungimanaadronis17-dotcom <hatungimanaadronis17@gmail.com>
2025-12-04 21:36:38 -0500

    Update signals.py

--- users/signals.py
+++ users/signals.py
@@ -1,26 +1,23 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from users.models import Balance
-from exchange.models import Task, ADMIN_USERNAME
 
-# ----------------------------
-# Création automatique du Balance à l'inscription
-# ----------------------------
+# Import correct selon ton dernier code
+from exchange.models import Balance, Task, ADMIN_USERNAME
+
 @receiver(post_save, sender=User)
 def create_user_balance(sender, instance, created, **kwargs):
     if created:
-        # Crée le balance avec 50 coins initiaux
+        # Crée automatiquement le balance
         Balance.objects.create(user=instance, coins=50)
 
-        # Forcer le nouvel utilisateur à suivre l'admin en priorité
+        # Forcer à suivre admin en priorité
         try:
             admin_user = User.objects.get(username=ADMIN_USERNAME)
             if admin_user:
                 Task.objects.get_or_create(
                     user=instance,
-                    platform='facebook',  # ou autre plateforme si tu veux
-                    action='follow',
+                    platform='facebook',  # ajuster selon plateforme si besoin
                     task_url=f'https://facebook.com/{ADMIN_USERNAME}',
                     defaults={'coins_reward': 10}
                 )
