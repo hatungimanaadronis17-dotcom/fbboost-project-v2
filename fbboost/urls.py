b0b5ca593cfce02ce1b454526b50d8e8044fe4ee
hatungimanaadronis17-dotcom <hatungimanaadronis17@gmail.com>
2025-12-05 21:09:35 -0500

    Update urls.py

--- fbboost/urls.py
+++ fbboost/urls.py
@@ -5,11 +5,16 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('django.contrib.auth.urls')),
-    path('', include('users.urls')),
-    path('', include('exchange.urls')),
+    
+    # Authentification Django (login, logout, password reset…)
+    path('accounts/', include('django.contrib.auth.urls')),
+    
+    # Tes applications
+    path('', include('users.urls')),       # page d'accueil, profil, etc.
+    path('', include('exchange.urls')),    # ici vont aller toutes les routes de boost
 ]
 
+# Servir les médias en développement
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
