e43450f00c0096173bb70bd43339545fbb87a26f
hatungimanaadronis17-dotcom <hatungimanaadronis17@gmail.com>
2025-12-05 22:13:35 -0500

    Update wsgi.py

--- fbboost/wsgi.py
+++ fbboost/wsgi.py
@@ -1,7 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
-# 🔧 Exécuter le fix admin une seule fois
-import fbboost.fix_admin
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fbboost.settings')
 
 application = get_wsgi_application()
