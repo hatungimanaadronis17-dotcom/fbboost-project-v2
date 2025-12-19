12c9de460792429e149c8d06b39777d548c5dc76
hatungimanaadronis17-dotcom <hatungimanaadronis17@gmail.com>
2025-12-19 16:53:44 -0500

    Update urls.py

--- exchange/urls.py
+++ exchange/urls.py
@@ -1,10 +1,17 @@
+# exchange/urls.py
+
 from django.urls import path
 from . import views
 
 app_name = 'exchange'
 
 urlpatterns = [
+    # Page principale de l'échange (celle avec les boutons Facebook, TikTok, etc.)
     path('', views.home, name='home'),
+
+    # URL appelée par le formulaire AJAX du site web
+    path('submit/', views.submit_task, name='submit_task'),
+
+    # URL pour l'app Android (si tu l'utilises toujours)
     path('gagner-coins/', views.gagner_coins, name='gagner_coins'),
-    path('submit-task/', views.submit_task, name='submit_task'),
 ]
