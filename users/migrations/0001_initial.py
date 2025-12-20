9632dd9045281bcedf2a0ebd8b91e685a88e3616
hatungimanaadronis17-dotcom <hatungimanaadronis17@gmail.com>
2025-12-19 22:52:35 -0500

    Update 0001_initial.py

--- users/migrations/0001_initial.py
+++ users/migrations/0001_initial.py
@@ -16,8 +16,6 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('auto_boost', models.BooleanField(default=True)),
                 ('boost_delay', models.IntegerField(default=30)),
-                ('coins', models.IntegerField(default=0)),
-                ('boosts', models.IntegerField(default=0)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('user', models.OneToOneField(on_delete=models.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
