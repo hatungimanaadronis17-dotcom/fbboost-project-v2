ed36acc6d89ad78d99b3cea9c8b8f054527a7f9e
hatungimanaadronis17-dotcom <hatungimanaadronis17@gmail.com>
2025-12-25 23:06:28 -0500

    Create migration to merge 0001 and 0002

---
+++ users/migrations/0003_merge_0001_0002.py
@@ -0,0 +1,8 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('users', '0001_initial'),
+        ('users', '0002_balance_task_withdrawal'),
+    ]
+    operations = []
