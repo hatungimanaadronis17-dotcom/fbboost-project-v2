c891b5a239c5f4284d33e3a3bdec627adef4235d
hatungimanaadronis17-dotcom <hatungimanaadronis17@gmail.com>
2026-01-28 17:49:43 -0500

    Add terms acceptance checkbox to registration form

--- users/forms.py
+++ users/forms.py
@@ -3,8 +3,22 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'placeholder': 'Ton email (jamais visible)'}))
-    
+    accept_terms = forms.BooleanField(
+        required=True,
+        label="J'accepte les conditions d'utilisation et la politique de confidentialité",
+        error_messages={
+            'required': "Vous devez accepter les conditions pour vous inscrire."
+        },
+        widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+    )
+
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ['username', 'email', 'password1', 'password2', 'accept_terms']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'form-control'})
+        self.fields['email'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password1'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control'})
