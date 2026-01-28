20e37acfd3d80b84386d491a60e3daf2ca90c301
hatungimanaadronis17-dotcom <hatungimanaadronis17@gmail.com>
2026-01-28 11:49:23 -0500

    Refactor migration for Balance model creation

--- exchange/migrations/0001_initial.py
+++ exchange/migrations/0001_initial.py
@@ -4,23 +4,21 @@ from django.conf import settings
 from django.db import migrations, models
 import django.db.models.deletion
 
+
 class Migration(migrations.Migration):
+
     initial = True
-    dependencies = [migrations.swappable_dependency(settings.AUTH_USER_MODEL)]
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
     operations = [
         migrations.CreateModel(
             name='Balance',
             fields=[
-                ('id', models.BigAutoField(...)),
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('coins', models.PositiveIntegerField(default=50)),
-                ('user', models.OneToOneField(..., related_name='balance', ...)),
-            ],
-        ),
-    ]
-                )),
-                ('coins', models.PositiveIntegerField(default=0)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('updated_at', models.DateTimeField(auto_now=True)),
                 ('user', models.OneToOneField(
                     on_delete=django.db.models.deletion.CASCADE,
                     related_name='balance',
