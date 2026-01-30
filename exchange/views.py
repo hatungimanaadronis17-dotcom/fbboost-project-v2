40524c6720fe3b587883bf96c79ff4f9bddbed1a
hatungimanaadronis17-dotcom <hatungimanaadronis17@gmail.com>
2026-01-29 19:21:48 -0500

    Add CSRF exemption decorator to views

--- exchange/views.py
+++ exchange/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.db import transaction
