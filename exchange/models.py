753f0ae98d74869e956fbd28d69d82472b2cf84c
hatungimanaadronis17-dotcom <hatungimanaadronis17@gmail.com>
2026-02-05 17:17:35 -0500

    Set default tx_type to 'credit' in models.py

--- exchange/models.py
+++ exchange/models.py
@@ -87,6 +87,7 @@ class Transaction(models.Model):
     tx_type = models.CharField(
         max_length=20,
         choices=TRANSACTION_TYPES,
+        default='credit',  
         verbose_name=_("Type")
     )
     coins = models.IntegerField(
